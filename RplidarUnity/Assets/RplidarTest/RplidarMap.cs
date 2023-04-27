bf9c046cf2f6f08ffe8253da463322fccac3aedd
PleyoProd <florentin.eraud@pleyo.fr>
2023-04-27 11:56:49 +0200

    Implemented this in unity demo project

--- RplidarUnity/Assets/RplidarTest/RplidarMap.cs
+++ RplidarUnity/Assets/RplidarTest/RplidarMap.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Threading;
+using System.Linq;
 
 [RequireComponent(typeof(MeshFilter))]
-public class RplidarMap : MonoBehaviour {
+public class RplidarMap : MonoBehaviour
+{
+    public bool filter = false;
+    [Tooltip("(3,10);(0,30) For Trampo space usual lidar spot")]
+    public Vector2 filteringDistance,filteringAngle;
 
+    public GameObject _prefab;
     public bool m_onscan = false;
 
     private LidarData[] m_data;
-    public string COM = "COM3";
+    public Settings _settings;
 
     public Mesh m_mesh;
     private List<Vector3> m_vert;
@@ -20,8 +26,12 @@ public class RplidarMap : MonoBehaviour {
 
     private Thread m_thread;
     private bool m_datachanged = false;
-	void Start () {
-
+    #region string
+    private string data= "";
+    #endregion
+    void Start()
+    {
+        RplidarBinding.ReleaseDrive();
         m_meshfilter = GetComponent<MeshFilter>();
 
         m_data = new LidarData[720];
@@ -31,11 +41,13 @@ public class RplidarMap : MonoBehaviour {
         for (int i = 0; i < 720; i++)
         {
             m_ind.Add(i);
+            var cross = Instantiate(_prefab, Vector3.zero, Quaternion.identity, transform);
+            _cross.Add(cross);
         }
         m_mesh = new Mesh();
         m_mesh.MarkDynamic();
 
-        RplidarBinding.OnConnect(COM);
+        RplidarBinding.OnConnect(_settings);
         RplidarBinding.StartMotor();
         m_onscan = RplidarBinding.StartScan();
 
@@ -47,29 +59,51 @@ public class RplidarMap : MonoBehaviour {
         }
 
     }
+    public bool CheckCondition(Vector3 pos, float theta)
+    {
+        if (!filter)
+            return true;
+        return pos.magnitude > filteringDistance.x && pos.magnitude < filteringDistance.y 
+            && theta>filteringAngle.x && theta<filteringAngle.y;
+    }
 
     void OnDestroy()
     {
         m_thread.Abort();
 
-        RplidarBinding.EndScan();
-        RplidarBinding.EndMotor();
-        RplidarBinding.OnDisconnect();
-        RplidarBinding.ReleaseDrive();
+        RplidarTest.CleanLidarExit(true);
 
         m_onscan = false;
     }
-
+    private List<GameObject> _cross = new List<GameObject>();
+    private List<float> updateDelays = new List<float>();
+    private float _lastUpdate;
     void Update()
     {
         if (m_datachanged)
         {
+            float delay = Time.time - _lastUpdate;
+            //We start avearging after 3 second
+            if(Time.time>3f)
+                updateDelays.Add(delay);
+            _lastUpdate = Time.time;
+            int NbOfPointsFiltered = 0;
             m_vert.Clear();
             for (int i = 0; i < 720; i++)
             {
-                m_vert.Add(Quaternion.Euler(0, 0, m_data[i].theta) * Vector3.right * m_data[i].distant*0.01f);
-
+                float theta = m_data[i].theta;
+                Vector3 Pos = Quaternion.Euler(0, 0, (float)theta) * Vector3.right * m_data[i].distant * 0.01f;
+                m_vert.Add(Pos * transform.localScale.x);
+                if (CheckCondition(Pos,theta))
+                {
+                    NbOfPointsFiltered++;
+                    _cross[i].SetActive(true);
+                }
+                else
+                    _cross[i].SetActive(false);
+                _cross[i].transform.position = Pos * transform.localScale.x;
             }
+            Debug.Log((delay) + " last update, average is : " + (updateDelays.Sum() / updateDelays.Count) + ", NbOfPoints filered : " + NbOfPointsFiltered);
             m_mesh.SetVertices(m_vert);
             m_mesh.SetIndices(m_ind.ToArray(), MeshTopology.Points, 0);
 
---
+++ RplidarUnity/Assets/Scale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    public float treshold = .2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            transform.localScale -= Vector3.one * treshold;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.localScale += Vector3.one * treshold;
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            ScaleChile(true);
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+            ScaleChile(false);
+    }
+
+    private void ScaleChile(bool up)
+    {
+        for (int i = 1; i < transform.childCount; i++)
+        {
+            var ch = transform.GetChild(i);
+            ch.localScale += Vector3.one * (up ? treshold : -treshold);
+        }
+    }
+}
